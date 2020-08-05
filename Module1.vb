d557068c8683b4830c521039777ba8c9be49ee93
Gloria Zheng <G.Zheng@ampleforth.org.uk>
2020-08-05 19:33:31 +0100

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim Meal, Drink, sum As Integer
+
+        Console.Write("Enter the price of a meal:")
+
+        Meal = Console.ReadLine
+
+        Console.Write("Enter the price of a drink:")
+
+        Drink = Console.ReadLine
+
+        sum = Meal + Drink * 2
+
+        Console.WriteLine(Meal & " + " & Drink & " *2 " & " = " & sum)
+
+        Console.Read()
+
+    End Sub
+
+End Module
